011cc6a0ab0f9a106ffbe713dfe96ebc867389b3
asserthq <sanya2003ogorodov@gmail.com>
2024-09-17 18:06:08 +0700

    feat: add admin page controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\MainController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function() {
+    Route::get('/', [MainController::class,'index'])->name('admin.index');
 });
