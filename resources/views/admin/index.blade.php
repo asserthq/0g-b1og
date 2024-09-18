6958eed2ede3481928102c48d89107f1d99b75c6
asserthq <sanya2003ogorodov@gmail.com>
2024-09-18 12:44:33 +0700

    refactor: make blade files like in laravel-anminlte usage guide

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,36 +1,26 @@
-{{-- <!DOCTYPE html>
-<html>
+@extends('admin.app')
 
-<head>
-    <meta charset="utf-8">
-    <title>OG-BLOG</title>
-</head>
+{{-- Customize layout sections --}}
 
-<body>
-    <h1>Admin page</h1>
-</body>
+@section('subtitle', 'Admin panel')
+@section('content_header_title', 'Admin panel')
+@section('content_header_subtitle', 'Index page')
 
-</html> --}}
+{{-- Content body: main page content --}}
 
-@extends('adminlte::page')
-
-@section('title', 'Dashboard')
-
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
-
-@section('content')
+@section('content_body')
     <p>Welcome to this beautiful admin panel.</p>
 @stop
 
-@section('css')
+{{-- Push extra CSS --}}
+
+@push('css')
     {{-- Add here extra stylesheets --}}
     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
+@endpush
 
-@section('js')
-    <script>
-        console.log("Hi, I'm using the Laravel-AdminLTE package!");
-    </script>
-@stop
+{{-- Push extra scripts --}}
+
+@push('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@endpush
