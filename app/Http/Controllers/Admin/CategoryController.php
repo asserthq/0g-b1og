287c33acb3cce673a7054218845c8afec8e08013
asserthq <sanya2003ogorodov@gmail.com>
2024-09-24 15:57:03 +0700

    feat: categories page resource add

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return 'Categories';
+        return view('admin.categories.index');
     }
 
     /**
