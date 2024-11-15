5131951eb02f1b7048dd205884977cfca0083825
asserthq <sanya2003ogorodov@gmail.com>
2024-11-15 19:40:14 +0700

    style: rename and move admin panel page blade

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        dd($request)->all();
+    }
+}
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layout')
+@extends('layouts.admin')
 
 {{-- Customize layout sections --}}
 
