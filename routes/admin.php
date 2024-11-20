6dc6cd29bcfd150e308419998ceed479ff48dbc4
asserthq <sanya2003ogorodov@gmail.com>
2024-11-20 14:03:24 +0700

    feature: add roles and middleware for admin panel

--- routes/admin.php
+++ routes/admin.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\PostController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function() {
-    Route::get('/', [MainController::class,'index'])->name('admin.index');
-    Route::resource('/categories', CategoryController::class);
-    Route::resource('/tags', TagController::class);
-    Route::resource('/posts', PostController::class);
+Route::prefix('admin')
+    ->middleware(['role:admin'])
+    ->group(function() {
+        Route::get('/', [MainController::class,'index'])->name('admin.index');
+        Route::resource('/categories', CategoryController::class);
+        Route::resource('/tags', TagController::class);
+        Route::resource('/posts', PostController::class);
 });
