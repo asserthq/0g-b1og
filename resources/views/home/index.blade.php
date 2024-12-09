f9f8678facac3bbd0886cfc6fdbc23267c5c4362
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-12-09 23:36:21 +0700

    refactor: move out from home page blade all imports except font-awesome

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,10 +7,8 @@
     <meta name="author" content="David Grzyb">
     <meta name="description" content="">
 
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
 
-    <!-- AlpineJS -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
 </head>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, preprocessCSS } from 'vite';
 import laravel from 'laravel-vite-plugin';
-//import path from 'path';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'],
             refresh: true,
         }),
     ],
-    // resolve: {
-    //     alias: {
-    //         '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
-    //     }
-    // }
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~fontawesome': path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free')
+        }
+    },
+    css: {
+        preprocessorOptions: {
+          scss: {
+            api: 'modern-compiler' // or "modern"
+          }
+        }
+    }
 });
