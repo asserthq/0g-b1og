2665d51e20c4250b73f9990f2d438f3abd49e62f
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-12-09 22:44:02 +0700

    feature: add frontend tailwind template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
+
+import * as bootstrap from 'bootstrap';
+
+import jQuery from 'jquery';
+window.$ = jQuery;
