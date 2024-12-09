b20acec56345a4bcae5e290e90d18589889ec9b1
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-12-09 23:37:16 +0700

    refactor: move out from post page blade all imports except font-awesome

--- resources/views/home/post.blade.php
+++ resources/views/home/post.blade.php
@@ -7,18 +7,8 @@
     <meta name="author" content="David Grzyb">
     <meta name="description" content="">
 
-    <!-- Tailwind -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
 
-        .font-family-karla {
-            font-family: karla;
-        }
-    </style>
-
-    <!-- AlpineJS -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
 </head>
