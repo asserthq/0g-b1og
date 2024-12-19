3d085a838a73b7fafba90d4c44c743e450f6a38d
asserthq <sanya2003ogorodov@gmail.com>
2024-12-19 23:05:27 +0700

    refactor: get category posts with less code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,14 +14,16 @@ class HomeController extends Controller
         $posts = Post::with('tags')->simplePaginate(5);
         return view("home.index", compact('posts'));
     }
-    public function blog($category_slug) 
+    public function blog($category_slug = null) 
     {
         $categories = Category::all();
         $posts =  null;
         if ($category_slug != null)
         {
-            $posts = Post::with('tags')
-                ->whereRelation('category', 'slug', '=', $category_slug)
+            $category = $categories->where('slug', $category_slug)->firstOrFail();
+            $posts = $category
+                ->posts()
+                ->with('tags')
                 ->orderBy('created_at', 'desc')
                 ->simplePaginate(5);
         }
