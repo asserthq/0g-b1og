4876088c9d5eef93bb9e5d44f1d2ceaf4cf4d926
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-12-24 01:41:37 +0700

    feature: working links to categories in blog page

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -19,7 +19,7 @@
 
                         @foreach ($categories as $category)
                             <li class="my-3">
-                                <a href="#"
+                                <a href="{{ route('home.blog.category', $category->slug) }}"
                                     class="px-3 py-2 text-sm font-medium {{-- uppercase --}} text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
                                     aria-label="View posts tagged">
 
