4a07bab93a46c70408dfeb8d2f89bd0e67bfb642
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-12-24 03:26:00 +0700

    feature: add scroll-to-top button

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,9 +1,5 @@
 <!DOCTYPE HTML>
-<html 
-    lang="en" 
-    class="scroll-smooth" 
-    :class="{ 'dark': darkMode }" 
-    x-data="{ showMenu: false, darkMode: false }">
+<html lang="en" class="scroll-smooth" :class="{ 'dark': darkMode }" x-data="{ showMenu: false, darkMode: false }">
 
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="apple-touch-icon" sizes="76x76" href="{{ Vite::asset('resources/favicons/apple-touch-icon.png') }}" />
@@ -15,6 +11,7 @@
 
 <body class="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
     <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+
         <div class="flex h-screen flex-col justify-between font-sans">
 
             @include('components.header')
@@ -25,6 +22,7 @@
 
             @include('components.footer')
 
+            @include('components.scroll-top')
         </div>
     </section>
 </body>
